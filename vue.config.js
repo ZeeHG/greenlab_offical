7f362289dbc31428e31dca67d9bb0c3d179dedf7
mousemen <778992212@qq.com>
2024-07-29 17:53:40 +0800

    cname

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
-// vue.config.js
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production"
-      ? "/greenlab_offical/" // 替换为你的仓库名
-      : "/",
-};
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/greenlab_offical/', 
+  outputDir: 'docs', 
+  assetsDir: 'static', 
+})
