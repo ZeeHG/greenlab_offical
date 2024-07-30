8fadfcb1778d6c2737edafba37d3b3ae9b9e25e0
mousemen <778992212@qq.com>
2024-07-30 13:46:21 +0800

    Rebuild and deploy

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,15 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = {
-  publicPath: '/',
-  outputDir: 'docs',
+  publicPath: '/greenlab_offical/',
+  outputDir: path.join('docs', 'greenlab_offical'),
   assetsDir: 'static',
   configureWebpack: {
     plugins: [
       {
         apply: (compiler) => {
           compiler.hooks.done.tap('AddCNAMEPlugin', (stats) => {
-            const cnamePath = path.join(__dirname, 'docs', 'CNAME');
+            const cnamePath = path.join(__dirname, 'docs', 'greenlab_offical', 'CNAME');
             fs.writeFileSync(cnamePath, 'greenlabimmi.com');
           });
         },
